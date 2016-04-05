aa5a84a625fc10001bfaeb9d3687c85c8340a960
Bob van Schendel <bobyantius@hotmail.com>
2016-04-05 13:22:36 +0200

    small commit with the basic graph drawing stuff

---
+++ PRJ3 Data Visualization/Coroutine.fs
@@ -0,0 +1,59 @@
+﻿module Coroutine
+
+open System
+
+type Cor<'a, 's> = 's -> CorStep<'a, 's> //takes an 's (whatever it is) and returns either a DONE with a result and a new state (Done of 'a, 's) OR yield of new(Yield of new )
+and CorStep<'a, 's> =
+  | Done of 'a* 's
+  | Yield of Cor<'a, 's>*'s
+
+let ret x = fun s -> Done(x, s)
+
+let rec bind p k =
+  fun s ->
+    match p s with
+    | Done(x, s') -> k x s'
+    | Yield(p', s') -> Yield((bind p' k), s')
+
+type CoroutineBuilder() =
+  member this.Return(x) = ret x
+  member this.ReturnFrom(c) = c
+  member this.Bind(p,k) = bind p k
+  member this.Zero() = ret ()
+let cor = CoroutineBuilder()
+
+let yield_ = fun s -> Yield((fun s -> Done((), s)), s)
+
+let getState =
+  fun s ->
+    Done(s, s)
+
+let setState x =
+  fun s ->
+    Done((), x)
+
+let rec repeat_ c =
+  cor{
+    do! c
+    do! yield_
+    return! repeat_ c
+  }
+
+
+let wait interval =
+  let currentTime = fun s -> Done(DateTime.Now, s)
+  cor{
+    let! t0 = currentTime
+    let rec wait() =
+      cor{
+        let! tc = currentTime
+        let dt = (tc - t0).TotalSeconds
+        if dt > interval then
+          do! yield_
+          return ()
+        else
+          do! yield_
+          return! wait()
+      }
+    do! wait()
+  }
---
+++ PRJ3 Data Visualization/Program.fs
@@ -0,0 +1,71 @@
+﻿open System
+open Coroutine
+
+//let sampledataGenerator x = [for y in 0.0 .. 0.05 .. 15.0 -> 10.0*(sin y - 20.0]
+
+//console settings
+Console.WindowWidth <- 150
+Console.WindowHeight <- 54
+
+let rec DrawGraph yadd =
+  Console.Clear()
+  for x in 1..149 do
+    let sine = int (25.0*sin (((float x) + yadd)/10.0)) + 26
+    if sine < 52 || sine > 1 then
+      Console.SetCursorPosition(x, sine)
+      Console.Write('X')
+
+let rec drawloop yadd =
+    let mutable buffer = ""
+    for y in 1..52 do
+      for x in 1..150 do
+        let sine = int (25.0*sin (((float x) + yadd)/10.0)) - 26
+        if x = 1 || y = 1 || y = 52 || x = 150 then
+          buffer <- buffer + "="
+        else // extra for COS and TAN|| int (10.0*cos (((float x) + yadd)/10.0)) - 10 = (y-52) || int (10.0*tan (((float x) + yadd)/10.0)) - 20 = (y-52)
+          if sine = (y-52)  then //we take the SIN of the x axis and enlarge it and give it an offset, then we check that with equality with y, if true then we draw "X" else we draw " "
+            buffer <- buffer + "X"
+          else
+            buffer <- buffer + " "
+    Console.Clear()
+    printfn "%s" buffer
+
+let plotA() =
+  cor{
+    let! s = getState//get the state
+    do drawloop s//draw a new graph with the state
+    return ()//increment the state so we get a different graph from the last one
+  }
+
+let plotB() =
+  cor{
+    let! s = getState
+    do DrawGraph s
+    return ()
+  }
+
+let IncrState() =
+  cor{
+    let! s = getState
+    do! setState (s - 2.0)
+    return ()
+  }
+  
+let A() =
+  cor{
+    do! wait 0.05
+    do! plotB()
+    do! IncrState()
+  } |> repeat_
+
+let rec costep c s =
+  match c s with
+  | Done(x, s') -> costep (cor{return x}) s'
+  | Yield(c', s') -> costep c' s'
+
+let sampleprogram() beginstate =
+  costep (A()) beginstate
+
+do sampleprogram() 0.0
+
+//do drawloop 0.3
