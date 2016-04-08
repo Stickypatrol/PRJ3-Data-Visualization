4e3d3bd453871ccd6f362715e25ae1610262485d
Mustafa Kocakaya <kocakaya_35@hotmail.com>
2016-04-08 13:24:25 +0200

    Connection to database

--- PRJ3 Data Visualization/Program.fs
+++ PRJ3 Data Visualization/Program.fs
@@ -4,6 +4,29 @@ open CSVParser
 open AuxTypes
 open ParserMonad
 open System.IO
+open System.Data
+open System.Data.Linq
+open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+//let sampledataGenerator x = [for y in 0.0 .. 0.05 .. 15.0 -> 10.0*(sin y - 20.0]
+
+let [<Literal>]connectionStr = "server=145.24.200.232;user=mustafa;database=test;password=root"
+let query = "INSERT INTO `data`(`id`, `data`) VALUES (null, 'test')"
+let connection = new MySql.Data.MySqlClient.MySqlConnection(connectionStr)
+do connection.Open()
+
+let command = new MySql.Data.MySqlClient.MySqlCommand(query,connection)
+command.ExecuteNonQuery() |> ignore
+
+let reader = command.ExecuteReader()
+let data = reader |> Seq.unfold (fun (reader) ->
+    if reader.Read() then
+        Some (List.init reader.FieldCount (fun i -> reader.GetValue(i)), reader)
+    else
+        None) |> Seq.toList
+printfn "%A" data
+connection.Close()
+connection.Dispose()
 
 let delimiter = ','
 
@@ -47,4 +70,4 @@ let rec costep c s =
   | Done(x, s') -> costep (cor{return x}) s'
   | Yield(c', s') -> costep c' s'
 
-do costep (ReadFunction() 25) []
+//do costep (ReadFunction() 25) []
