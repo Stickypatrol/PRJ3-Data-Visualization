197011edc87e7c811e4eecd8747e070165fe4757
Bob van Schendel <bobyantius@hotmail.com>
2016-04-08 12:27:02 +0200

    made a small commit with test 25 queries program stuff

--- PRJ3 Data Visualization/Program.fs
+++ PRJ3 Data Visualization/Program.fs
@@ -13,7 +13,7 @@ let parseFunc data =
     | Success(result, s') -> Done(result,s)
     | Fail(_) -> failwith "lexing failed"
 
-let ReadFunction () =
+let ReadFunction () i (*i is a counter for number of queries*)=
   let StartReading =
     fun s ->
       let sr = new StreamReader (@"C:\samplepics\fietsdiefstaldata.csv")
@@ -21,26 +21,30 @@ let ReadFunction () =
   let getLine (sr:StreamReader) =
     fun s ->
       Done(sr.ReadLine(), s)
-  let rec RecursiveReader reader =
+  let rec RecursiveReader reader i =
     cor{
-      let! newline = getLine reader
-      let! parsedLine = parseFunc (newline |> List.ofSeq) //list with 1 entry, which is a list of CSV tokens
-      //insert the insert query here as a coroutine, then just repeat it until it returns yield
-      do printfn "%A" parsedLine
-      do! yield_ //we can add a pause here for whichever reason
-      if reader.EndOfStream then
-        return ()
+      if i > 0 then
+        let! newline = getLine reader
+        let! parsedLine = parseFunc (newline |> List.ofSeq) //list with 1 entry, which is a list of CSV tokens
+        //insert the insert query here as a coroutine, then just repeat it until it returns yield
+        do printfn "%A" parsedLine
+        do! yield_ //we can add a pause here for whichever reason
+        if reader.EndOfStream then
+          return ()
+        else
+          return! RecursiveReader reader (i-1)
       else
-        return! RecursiveReader reader
+        return ()
     }
   cor{
     let! reader = StartReading
-    do! RecursiveReader reader
-  } |> repeat_
+    do! RecursiveReader reader i
+    do! yield_
+  }
 
 let rec costep c s =
   match c s with
   | Done(x, s') -> costep (cor{return x}) s'
   | Yield(c', s') -> costep c' s'
 
-do costep (ReadFunction()) []
+do costep (ReadFunction() 25) []
